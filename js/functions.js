ac3a10ff53533891858eea2769e5c91b6d348141
シモン <simon@azet.jp>
2016-10-17 10:50:29 +0900

    Unified scrolling and removed the need of a picture.
    Works in more browsers, including Firefox.
    Also compatible within Wordpress.

--- js/functions.js
+++ js/functions.js
@@ -9,17 +9,17 @@
  * @return array {width, height}
  */
 function viewport() {
-  // exception
-  if(/iPad/.test(navigator.userAgent)) {
-    return { width: document.width, height: document.height};
-  }
+	// exception
+	if(/iPad/.test(navigator.userAgent)) {
+		return { width: document.width, height: document.height};
+	}
 
-  var e = window, a = 'inner';
-  if (!('innerWidth' in window )) {
-    a = 'client';
-    e = document.documentElement || document.body;
-  }
-  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+	var e = window, a = 'inner';
+	if (!('innerWidth' in window )) {
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
 }
 
 
@@ -27,81 +27,77 @@ function viewport() {
  * top arrow (scrool to top)
  * depends on:
  * - jQuery
- * - [.page] element to reference top position (if topScroll negative, topArray will be displayed)
+ * The button to be displayed is #TopArrowScrollBtn
  * CSS sample for the button:
- * #scrollBtn {
- *  position: fixed;
+ * #TopArrowScrollBtn {
+ * 	position: fixed;
  * 	right: 10px;
  * 	bottom: 10px;
  * 	padding: 10px;
  * 	background-color: rgba(0, 0, 0, 0.8);
- *  border-radius: 5px;
- *  color: white;
- *  font-weight: bold;
+ * 	border-radius: 5px;
+ * 	color: white;
+ * 	font-weight: bold;
+ * 	text-decoration: none;
  * }
  * usage: 
- * - TopArray.init();
+ * - TopArrow.init();
  * tested on:
  * - IE 9,11
+ * - Firefox 49
  * - Edge
- * - PC Chrome
+ * - PC Chrome 53
  * - iPad (iOS 9)
  * - Android 7 Chrome
  */
 var TopArrow = {
-  // REFERENCE ELEMENT to activate the button on scroll
-  topMenu: $(".page")[0],
+	init: function() {
+		// arrow itself
+		var topArrow = jQuery('<a id="TopArrowScrollBtn" style="display: none;" href="#scroll">^</a>');
+		// button event
+		topArrow.click(this.doScroll);
 
-  init: function() {
-    if(!this.topMenu) {
-      // no scrolling position reference found
-      // we stop here to prevent js errors bellow
-      return;
-    }
+		// appending element to the page
+		jQuery('body').append(topArrow);
 
-    // arrow itself
-    var topArrow = $('<a id="scrollBtn" style="display: none;" href="#scroll"><img src="images/scrolltop-icon2.png"/></a>');
-    // button event
-    topArrow.click(this.doScroll);
-
-    // appending element to the page
-    $('body').append(topArrow);
-
-    // scroll event handler
-    $(document).scroll(this.checkScroll);
-  },
+		// scroll event handler
+		jQuery(document).scroll(this.checkScroll);
+	},
 
 
-  // jQuery event
-  doScroll: function(e_) {
-    e_.preventDefault();
-    // possible to have different buttons with different actions
-    if($(this).attr('href')==='#scroll') {
-      var offset = ($(document).scrollTop());
-      if(/(IE 9|Trident)/.test(navigator.userAgent)) {
-        // IE 9,11 only
-        $('body, html').animate({scrollTop: '-'+offset+'px'});
-      }
-      else {
-        // others
-        $('body').animate({scrollTop: '-'+offset+'px'});
-      }
-    }
-  },
+	// jQuery event
+	doScroll: function(e_) {
+		e_.preventDefault();
+		// possible to have different buttons with different actions
+		if(jQuery(this).attr('href')==='#scroll') {
+			//var offset = (jQuery(document).scrollTop());
+			//if(/(IE 9|Trident)/.test(navigator.userAgent)) {
+			//	// IE 9,11 only
+			//	jQuery('body, html').animate({scrollTop: '-'+offset+'px'});
+			//}
+			//else if(/Firefox/.test(navigator.userAgent)) {
+			//	jQuery('body, html').animate({scrollTop: 0});
+			//} else {
+			//	// others
+			//	//jQuery('body').animate({scrollTop: '-'+offset+'px'});
+			//}
+			jQuery('body, html').animate({scrollTop: 0});
+		}
+	},
 
 
-  // scroll check function
-  // jQuery event
-  checkScroll: function () {
-    var topscroll = TopArrow.topMenu.getBoundingClientRect().top;
-    console.log( topscroll );
-    if(topscroll<0) {
-      // show
-      $('#scrollBtn').fadeIn('fast');
-    }
-    else {
-      // hide
-      $('#scrollBtn').fadeOut('fast');
-    }
-  }
+	// scroll check function
+	// jQuery event
+	checkScroll: function () {
+		var topscroll = jQuery(document).scrollTop();
+		if(topscroll>0) {
+			// show
+			jQuery('#TopArrowScrollBtn').fadeIn('fast');
+		}
+		else {
+			// hide
+			jQuery('#TopArrowScrollBtn').fadeOut('fast');
+		}
+	}
 };
+
